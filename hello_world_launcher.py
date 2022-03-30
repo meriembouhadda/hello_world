1d168a9bb5a7d59b831f935eb1839776cacbb833
meriembouhadda <102356117+meriembouhadda@users.noreply.github.com>
2022-03-30 17:19:12 +0200

    Update hello_world_launcher.py

--- hello_world_launcher.py
+++ hello_world_launcher.py
@@ -9,7 +9,7 @@ def test():
     hello_component = kfp.dsl.ContainerOp(
         name='hello-world',
         image='docker.io/mariembouhadda/hello-world',
-        command=['python', 'kiki.py'],
+        command=['python', 'hello_world.py'],
         )
     return hello_component
 @kfp.dsl.pipeline(
