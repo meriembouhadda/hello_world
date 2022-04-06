9112c3540a7d65a8251ba6a58c900611fbe69314
meriembouhadda <mbouhadda@kheopsys.com>
2022-04-06 12:34:25 +0000

    last commit

--- hello_world_launcher.py
+++ hello_world_launcher.py
@@ -19,9 +19,9 @@ def hi():
     hello.execution_options.caching_strategy.max_cache_staleness = "P0D"
 kfp.compiler.Compiler().compile(hi, 'hello_world.zip')
 client = kfp.Client(host='https://21306363b1ebd301-dot-us-central1.pipelines.googleusercontent.com')
-EXPERIMENT_NAME = 'hello world dok'
+EXPERIMENT_NAME = 'hello world'
 
 experiment = client.create_experiment(name=EXPERIMENT_NAME)
-for i in range (1):
-    run = client.run_pipeline(experiment.id, 'hello world dok', 'hello_world.zip')
-    #client.run_pipeline(experiment.id, 'hello world docker', 'hello_world.zip')
+#for i in range (1):
+    #run = client.run_pipeline(experiment.id, 'hello world dok', 'hello_world.zip')
+client.run_pipeline(experiment.id, 'hello world', 'hello_world.zip')
